f7ced9ed8116632620a206d1146b814bc628d359
Crowdbotics <team@crowdbotics.com>
2021-03-31 13:13:52 +0000

    Update models

--- ybgrconnect_web_25383/wsgi.py
+++ ybgrconnect_web_25383/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ybgrconnect_web_25383.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ybgrconnect_web_25383.settings")
 
 application = get_wsgi_application()
